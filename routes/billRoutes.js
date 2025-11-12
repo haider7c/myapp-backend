b6c51150776516d327de1b15d589c3a9c1813116
haider7c <haider7c@gmail.com>
2025-11-12 20:12:10 +0500

    complete backend code (safe version)

--- routes/billRoutes.js
+++ routes/billRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Bill = require('../models/Bill.js');
+const Bill = require('../models/Bill');
 
 // Get all bills
 router.get('/', async (req, res) => {
