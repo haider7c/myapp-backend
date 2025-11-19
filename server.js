d96f54b401bc5875a07a7b33f2ce2e5edbc94215
haider7c <haider7c@gmail.com>
2025-11-19 10:09:58 +0500

    mongo uri correction

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 console.log("Mongo URI:", process.env.MONGODB_URI);
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ MongoDB error:", err));
 
